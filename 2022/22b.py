eab8a0264b85d7625dc246853406be3fc9b0d8b3
diegorodriguezv <diegorodriguezv@gmail.com>
2023-08-24 13:09:01 -0500

    Day 22: cleanup and documentation

--- 2022/22b.py
+++ 2022/22b.py
@@ -1,13 +1,33 @@
 import fileinput
-import pprint
 
+""" The solution to this problem needs several concepts that represent ideas in 
+different levels of abstraction.
+Map: 2D representation of the cube. Composed of square faces.
+Face: Represented by it's top left coordinate relative to other faces. One unit is the
+    length of the side.
+Cube: A model of the 3D entity but without using a third dimension. It is modeled by 
+    assigning letters to each of it's vertices.
+Corners: Represent a face of the cube by enumerating the 4 vertices in a top-bottom,
+    left-right order.
+Facing: A direction in 2D space. Can also represent an edge of a face, in that case it
+    can be called a side.
+Direction: A tuple that represents movement or delta in 2D space.
+Seam: Is a translation from one position and one direction in a face to another position 
+    and another direction in other face. This abstraction allows easy navigation around 
+    the edges of a face that has no adjacent face to it.
+Turn: Is a change in direction or rotation. Represented by "L" (clockwise) and "R" 
+    (clockwise).
+Position: A 2D coordinate in the map.
+"""
+
+# These are the "facing" or directions
 RIGHT, DOWN, LEFT, UP = 0, 1, 2, 3
 NAME = {RIGHT: "RIGHT", DOWN: "DOWN", LEFT: "LEFT", UP: "UP"}
 
 
 def main():
     _map, instructions = get_map_and_instructions()
-    position, facing = walk(_map, parse_instructions(instructions))
+    position, facing = follow_instructions(_map, instructions)
     print(position, facing)
     row, col = position
     print(1000 * (row + 1) + 4 * (col + 1) + facing)
@@ -29,6 +49,7 @@ def adjacent_corners(cube, corners, facing):
 
 
 def corners_edge(corners, facing):
+    """Return the two corners of the face in the edge facing a given direction."""
     ul, ur, dl, dr = corners
     if facing == UP:
         edge = ul, ur
@@ -41,7 +62,8 @@ def corners_edge(corners, facing):
     return edge
 
 
-def side_facing(side):
+def seam_dir(side):
+    """Select a direction to traverse a side of a face."""
     if side == UP or side == DOWN:
         return RIGHT
     if side == LEFT or side == RIGHT:
@@ -62,17 +84,17 @@ def rotated_adjacent_corners(cube, corners, facing):
     but rotated so that the edge shared with the current face is located apropriately.
     """
     rotated = adjacent_corners(cube, corners, facing)
-    for rotations in range(4):
+    for _ in range(4):
         if corners_edge(corners, facing) == corners_edge(
             rotated, opposite_facing(facing)
         ):
-            return rotated, rotations
+            return rotated
         rotated = rotate_corners(rotated, "R")
-        rotations += 1
 
 
 def wrapped_edges(faces):
-    """Assign cube corners to each face (wrap) and return the edges for faces without an adjacent face in the map.
+    """Assign cube corners to each face (wrap) and return the edges for faces without an
+     adjacent face in the map.
     f----g
     | \  | \
     |   a----b
@@ -91,31 +113,28 @@ def wrapped_edges(faces):
     edges = {}
     # corners for each face in a cube (left -> right, top -> bottom)
     cube = list(map(tuple, ["abdc", "dcih", "ihfg", "fgab", "faid", "bgch"]))
-    work = [(faces[0], cube[0], 0)]
+    work = [(faces[0], cube[0])]
     while work:
-        face, corners, rotations = work.pop()
+        face, corners = work.pop()
         if face in visited:
             continue
         visited.add(face)
         for facing in [RIGHT, DOWN, LEFT, UP]:
             neighbor_face = adjacent_face(face, faces, facing)
             if neighbor_face:
-                neighbor_corners, rotations = rotated_adjacent_corners(
-                    cube, corners, facing
-                )
-                work.append((neighbor_face, neighbor_corners, rotations))
+                neighbor_corners = rotated_adjacent_corners(cube, corners, facing)
+                work.append((neighbor_face, neighbor_corners))
             else:
                 edges[(face, facing)] = corners_edge(corners, facing)
     return edges
 
 
 def start_point(_map):
-    first_row = _map[0]
-    first_col = first_row.index(".")
+    first_col = _map[0].index(".")
     return 0, first_col
 
 
-def walk(_map, instructions):
+def follow_instructions(_map, instructions):
     facing = 0
     position = start_point(_map)
     side_length = get_side_length(_map)
@@ -124,7 +143,7 @@ def walk(_map, instructions):
         if type(instruction) is int:
             position, facing = walk_straight(_map, position, facing, instruction, seams)
         else:
-            facing = change_facing(facing, instruction)
+            facing = rotate_facing(facing, instruction)
     return position, facing
 
 
@@ -155,11 +174,15 @@ def tile(_map, position):
 
 
 def get_side_length(_map):
+    """Calculate the side length of each face by dividing the total area of the map by
+    6 faces and taking the square root."""
     total_area = sum(1 for row in _map for char in row if char != " ")
     return int((total_area / 6) ** 0.5)
 
 
 def face_map(_map, side_length):
+    """Return a list of relative coordinates of faces in a map. One unit is the size
+    of a square face."""
     faces = []
     face_row = 0
     while face_row < len(_map):
@@ -173,27 +196,32 @@ def face_map(_map, side_length):
 
 
 def adjacent_face(face, faces, facing):
+    """Return the adjacent face in the map in a given direction or None if it doesn't
+    exist."""
     adjacent_face = add_tuples(face, facing_direction(facing))
     return adjacent_face if adjacent_face in faces else None
 
 
-def start_edge_coordinates(face, side, side_length, _dir):
-    """Returns the start coordinates of the side edge of a map face in
-    a given direction."""
+def start_seam_coordinates(face, side, side_length, seam_dir):
+    """Returns the start coordinates to traverse a seam in a given direction."""
     face_row, face_col = face
     top, left = face_row * side_length, face_col * side_length
     bottom, right = (face_row + 1) * side_length - 1, (face_col + 1) * side_length - 1
     if side == LEFT:
-        return (top, left) if _dir != UP else (bottom, left)
+        return (top, left) if seam_dir != UP else (bottom, left)
     if side == UP:
-        return (top, left) if _dir != LEFT else (top, right)
+        return (top, left) if seam_dir != LEFT else (top, right)
     if side == DOWN:
-        return (bottom, left) if _dir != LEFT else (bottom, right)
+        return (bottom, left) if seam_dir != LEFT else (bottom, right)
     if side == RIGHT:
-        return (top, right) if _dir != UP else (bottom, right)
+        return (top, right) if seam_dir != UP else (bottom, right)
 
 
 def create_seams(_map, side_length):
+    """Return the "seams" of the map. Seams is a dictionary where the key is a map
+    coordinate and a direction and the value is the new map coordinate and the new
+    direction. It represents the transitions or portals through which to walk in the
+    edges of faces that have no adjacent face."""
     edges = wrapped_edges(face_map(_map, side_length))
     paired = set()
     seams = {}
@@ -204,24 +232,19 @@ def create_seams(_map, side_length):
         if (pair_face, pair_side) in paired:
             continue
         paired.add((face, side))
-        seam_facing1 = side_facing(side)
-        pos1 = start_edge_coordinates(face, side, side_length, seam_facing1)
-        dir1 = facing_direction(seam_facing1)
-        seam_facing2 = side_facing(pair_side)
+        seam_dir1 = seam_dir(side)
+        pos1 = start_seam_coordinates(face, side, side_length, seam_dir1)
+        dir1 = facing_direction(seam_dir1)
+        seam_dir2 = seam_dir(pair_side)
         if edge != pair_edge:
-            seam_facing2 = opposite_facing(seam_facing2)
-        pos2 = start_edge_coordinates(pair_face, pair_side, side_length, seam_facing2)
-        dir2 = facing_direction(seam_facing2)
-        print(NAME[side], face, NAME[seam_facing1])
-        print(NAME[pair_side], pair_face, NAME[seam_facing2])
-        print()
+            seam_dir2 = opposite_facing(seam_dir2)
+        pos2 = start_seam_coordinates(pair_face, pair_side, side_length, seam_dir2)
+        dir2 = facing_direction(seam_dir2)
         for _ in range(side_length):
             seams[pos1, side] = pos2, opposite_facing(pair_side)
             seams[pos2, pair_side] = pos1, opposite_facing(side)
             pos1 = add_tuples(pos1, dir1)
             pos2 = add_tuples(pos2, dir2)
-    pprint.pp(seams)
-    print(len(seams))
     return seams
 
 
@@ -244,7 +267,7 @@ def parse_instructions(instructions):
     return parts
 
 
-def change_facing(facing, turn):
+def rotate_facing(facing, turn):
     if turn == "R":
         return (facing + 1) % 4
     if turn == "L":
@@ -283,7 +306,7 @@ def get_map_and_instructions():
             instructions = line
         else:
             _map.append(line)
-    return _map, instructions
+    return _map, parse_instructions(instructions)
 
 
 if __name__ == "__main__":
